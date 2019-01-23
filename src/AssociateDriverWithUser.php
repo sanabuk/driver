18b5788f893c435c3af93c0088a462e2d4896b89
sanabuk <guillaume.ferard@orange.fr>
2019-01-24 00:49:40 +0100

    Update AssociateDriverWithUser.php

--- src/AssociateDriverWithUser.php
+++ src/AssociateDriverWithUser.php
@@ -2,14 +2,29 @@
 
 namespace sanabuk\driver;
 
-class AssociateDriverWithModel extends Driver
-{
-	protected $model;
+use App\User;
+use sanabuk\driver\Driver;
+use Exception;
 
-	public function __construct(Model $model)
+class AssociateDriverWithUser extends Driver
+{
+	public function handler(Driver $driver, User $model)
 	{
-		parent::construct();
-		$this->model = model;
+		try{
+			$this->testEligibilityUser($model)
+			$driver->user()->associate($model);
+			$driver->save();
+		} catch (Exception $e){
+			throw $e;	
+		}
+	}
+
+	/**
+	 * Un user ne peut pas avoir plus de X drivers
+	 */
+	public function testEligibilityUser(User $model)
+	{
+		if($model->drivers->count > 1) throw new Exception("Error Processing Request", 422);
 	}
 
 }
