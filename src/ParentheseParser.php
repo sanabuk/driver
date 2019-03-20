653f0294f373260ad58e4859cddede0da300f3d2
sanabuk <guillaume.ferard@orange.fr>
2019-03-20 20:35:50 +0100

    cleaning

--- src/ParentheseParser.php
+++ src/ParentheseParser.php
@@ -11,61 +11,60 @@ class ParentheseParser
 
     public function generate($string, $open = '(', $close = ')')
     {
-        $query = "driver(equals:name=NAME,historic(driver(like:name=NAME),vehicle(like:name=CIT)),vehicle(like:brand=PEU)){id,vehicle{id,license_number,brand,color},historic{driver{name},vehicle{license_number}}}";
         $len_string = strlen($string);
-        $tokens = [];
-        for ($i=0; $i < $len_string; $i++) { 
+        $tokens     = [];
+        for ($i = 0; $i < $len_string; $i++) {
             $tokens[] = $string[$i];
         }
 
-        $naming = '';
+        $naming                = '';
         $this->previous_models = [];
-        $this->output = [];
-        $this->depht = 0;
+        $this->output          = [];
+        $this->depht           = 0;
 
         foreach ($tokens as $key => $value) {
-            if($key == $len_string-1){
-                switch($value){
+            if ($key == $len_string - 1) {
+                switch ($value) {
                     case $close:
-                    if($naming != ''){
+                        if ($naming != '') {
+                            $this->output = $this->addConstrains($naming);
+                            $naming       = '';
+                        }
+                        array_pop($this->previous_models);
+                        break;
+
+                    default:
+                        $naming .= $value;
                         $this->output = $this->addConstrains($naming);
-                        $naming = '';
-                    }
-                    array_pop($this->previous_models);
-                    break;
-                
-                default:
-                    $naming .= $value;
-                    $this->output = $this->addConstrains($naming);
-                    $naming = '';
-                    break;
+                        $naming       = '';
+                        break;
                 }
                 return $this->output;
             }
             switch ($value) {
                 case $open:
-                    $this->output = $this->createDepht($naming);
-                    $this->previous_models[]= $naming;
-                    $naming = '';
+                    $this->output            = $this->createDepht($naming);
+                    $this->previous_models[] = $naming;
+                    $naming                  = '';
                     $this->depht++;
                     break;
 
                 case ',':
-                    if($naming == ''){
+                    if ($naming == '') {
                         break;
                     }
                     $this->output = $this->addConstrains($naming);
-                    $naming = '';
+                    $naming       = '';
                     break;
 
                 case $close:
-                    if($naming != ''){
+                    if ($naming != '') {
                         $this->output = $this->addConstrains($naming);
-                        $naming = '';
+                        $naming       = '';
                     }
                     array_pop($this->previous_models);
                     break;
-                
+
                 default:
                     $naming .= $value;
                     break;
@@ -77,39 +76,36 @@ class ParentheseParser
 
     private function createDepht($string)
     {
-        if(empty($this->previous_models)){
-            $this->output[$string] = [];
-        } else {
+        switch (count($this->previous_models)) {
+            case 0:
+                $this->output[$string] = [];
+                break;
+
+            case 1:
+                $this->output[$this->previous_models[0]][$string] = [];
+                break;
+
+            case 2:
+                $this->output[$this->previous_models[0]][$this->previous_models[1]][$string] = [];
+                break;
+
+            case 3:
+                $this->output[$this->previous_models[0]][$this->previous_models[1]][$this->previous_models[2]][$string] = [];
+                break;
+
+            default:
+                # code...
+                break;
+                return $this->output;
+        }
+
+        private function addConstrains($string)
+        {
             switch (count($this->previous_models)) {
                 case 1:
-                    $this->output[$this->previous_models[0]][$string] = [];
+                    $this->output[$this->previous_models[0]][] = $string;
                     break;
 
-                case 2:
-                    $this->output[$this->previous_models[0]][$this->previous_models[1]][$string] = [];
-                    break;
-
-                case 3:
-                    $this->output[$this->previous_models[0]][$this->previous_models[1]][$this->previous_models[2]][$string] = [];
-                    break;
-                
-                default:
-                    # code...
-                    break;
-            }
-            
-        }
-        return $this->output;
-    }
-
-    private function addConstrains($string)
-    {
-        
-        if(count($this->previous_models) == 1){
-            $this->output[$this->previous_models[0]][] = $string;
-        } else {
-            
-            switch (count($this->previous_models)) {
                 case 2:
                     $this->output[$this->previous_models[0]][$this->previous_models[1]][] = $string;
                     break;
@@ -130,9 +126,7 @@ class ParentheseParser
                     $this->output[] = $string;
                     break;
             }
+            return $this->output;
         }
-        return $this->output;
+
     }
-
-
-}
